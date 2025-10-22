f32a0bb90c684b1fcc68f54ea99b220bfdbe0cbf
Darina Maier <darina.maier@epfedu.fr>
2025-10-22 16:54:27 +0200

    Création du TP0_calculette

---
+++ S3/TP0_calculette/src/tp0_calculette/TP0_calculette.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_calculette;
+import java.util.Scanner;
+/**
+ *
+ * @author maier
+ */
+public class TP0_calculette {
+
+    /**
+     * @param args the command line arguments
+     */
+    int operateur = 0; 
+    int  operande1 = 0;
+    int  operande2=0
+            
+    public static void main(String[] args) {
+        System.out.println( " Please enter the operator: 1) add " );
+        System.out.println( " Please enter the operator: 2) substract  " );
+        System.out.println( " Please enter the operator: 3) multiply " );
+        System.out.println( " Please enter the operator: 4) divide  " );
+        System.out.println( " Please enter the operator: 5) modulo   " );
+    String operateur; 
+    Scanner sc ; 
+    sc = new Scanner (System.in) ; 
+    System.out.println("Entre un nombre"); 
+    operateur = sc.nextLine(); 
+    
+    
+            
+    }
+    
+}
