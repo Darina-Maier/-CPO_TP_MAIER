48f6bc3cc67d0878e23453b4f9ce58050651af8d
Darina Maier <darina.maier@epfedu.fr>
2025-10-22 16:54:03 +0200

    Création du TP0_helloWorld

---
+++ S3/TP0_ helloWorld/src/tp0_/helloworld/TP0_HelloWorld.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_.helloworld;
+import java.util.Scanner;
+
+/** 
+ * date : 20/10/25
+ * nom : Maier Darina 
+ * Programme : TP0
+ * @author maier
+ */
+public class TP0_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println( " Bonjour " ) ; 
+        System.out.println( " Au revoir " );
+        String prenom; 
+Scanner sc ; 
+sc = new Scanner (System.in) ; 
+System.out.println("Quel est votre prénom ?"); 
+prenom = sc.nextLine(); 
+
+    }
+    
+   
+}
