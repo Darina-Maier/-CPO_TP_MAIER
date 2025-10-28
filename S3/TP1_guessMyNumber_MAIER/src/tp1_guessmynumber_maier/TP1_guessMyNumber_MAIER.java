4e35333522ebcc20d37944c14949e807aca29df5
Darina Maier <darina.maier@epfedu.fr>
2025-10-28 17:25:11 +0100

    TP1 exo 3

--- S3/TP1_guessMyNumber_MAIER/src/tp1_guessmynumber_maier/TP1_guessMyNumber_MAIER.java
+++ S3/TP1_guessMyNumber_MAIER/src/tp1_guessmynumber_maier/TP1_guessMyNumber_MAIER.java
@@ -20,10 +20,46 @@ public class TP1_guessMyNumber_MAIER {
         Scanner sc = new Scanner(System.in);
         Random generateurAleat = new Random();
         
-        System.out.println("Voici les 5 nombres aléatoires :");
-        for (int i =0; i<5; i++) {
-            int nb = generateurAleat.nextInt(101);
-            System.out.println(nb);
+        System.out.println("Choisissez un niveau de dificulté :facile / normal / difficile");
+        System.out.print("Entrez le niveau :");
+        String niveau = sc.next().toLowerCase();
+        
+        int tentativeMax = 0;
+        if (niveau.equals("facile")) {
+            tentativeMax = 20;
+        }
+        else if (niveau.equals("normal")){
+            tentativeMax = 10;
+        }
+        else if (niveau.equals("difficile")){
+            tentativeMax = 5;
+        }
+                
+        
+        int nombreAlea = generateurAleat.nextInt(101); 
+        int nombreUtilisateur = -1;
+        int cpt=0;
+        
+        System.out.println("Devinez un nombre entre 0 et 100");
+         
+        while(nombreUtilisateur != nombreAlea && cpt<tentativeMax) {
+            System.out.println("Entrez un nombre: ");
+            nombreUtilisateur = sc.nextInt();
+            cpt++;
+            
+            if (nombreUtilisateur < nombreAlea){
+                System.out.println("trop petit");
+            }
+            else if (nombreUtilisateur > nombreAlea){
+                System.out.println("trop grand");
+            }
+        }
+        if (nombreUtilisateur == nombreAlea){
+            System.out.println("Vous avez gagné");
+            System.out.println("Nombre de tentative :" +cpt);
+        }
+        else {
+            System.out.println("Vous avez perdu");
         }
     }
     
