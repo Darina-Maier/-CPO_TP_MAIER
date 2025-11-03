81f3e42db373a319d382da9cc4a49f5636dd5b68
Darina Maier <darina.maier@epfedu.fr>
2025-11-03 15:34:05 +0100

    TP 2 en cours

---
+++ S3/TP2_Bieres_MAIER/src/tp2_bieres_maier/BouteilleBiere.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_maier;
+
+/**
+ *
+ * @author maier
+ */
+public class BouteilleBiere {
+    String Nom;
+    double degreAlcool;
+    String brasserie; 
+    boolean ouverte; 
+    
+public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) { 
+    Nom = unNom; 
+    degreAlcool = unDegre; 
+    brasserie = uneBrasserie; 
+    ouverte = false; 
+}
+    
+    public void lireEtiquette() { 
+        System.out.println("Bouteille de " + Nom +" (" + degreAlcool + " degres) \nBrasserie : " + brasserie ) ; 
+    }  
+    
+    public boolean Decapsuler() {
+        if (ouverte == false) {
+            ouverte = true;
+            return ouverte;
+        } else {
+            System.out.print("erreur : biere déjà ouverte");
+            return false; 
+        }
+    }
+}
