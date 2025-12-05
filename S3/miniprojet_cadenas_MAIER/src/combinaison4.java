f61790bdd72802a9b7585a18647892504c72cb81
Darina Maier <darina.maier@epfedu.fr>
2025-12-05 15:38:46 +0100

    Mini projet 1 "cadenas" : classe crée + fin de séance

---
+++ S3/miniprojet_cadenas_MAIER/src/combinaison4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author maier
+ */
+public class combinaison4 {
+
+    public combinaison4() {
+        genererCombiAleat();
+    }
+
+    private void genererCombiAleat() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
